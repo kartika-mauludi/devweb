2b4b36b137425f714d903f695e237fea595be7ad
ludius01 <69234573+ludius01@users.noreply.github.com>
2025-03-29 13:00:23 +0700

    langganan-customer

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -26,17 +26,17 @@
     <div class="alert alert-info alert-dismissible">
       <div class="container">
         Anda Memiliki Tagihan Pembayaran Yang Belum Diselesaikan Klik Tombol Berikut Untuk Melihat Pembayaran Anda <p>
-          <button class="btn btn-primary my-2">Payment</button>
+          <a href="" class="btn btn-primary my-2">Payment</a>
         </p>
         </div>
     </div>
     @endif
 
-    @if(now()->diffInDays(\Carbon\Carbon::parse($sub->end_date)) <=1 && $payment->status == "completed")
+    @if(now()->diffInDays(\Carbon\Carbon::parse($sub->end_date)) <=3 && $payment->status == "completed")
     <div class="alert alert-dangery alert-dismissible">
       <div class="container">
               Waktu Langganan Anda Akan Segera Habis, Silahkan Perpanjang Waktu Langganan Anda <p>
-          <button class="btn btn-primary my-2">Perpanjang Langganan</button>
+          <a href="{{ route('customer/langganan.upgrade') }}" class="btn btn-primary my-2">Perpanjang Langganan</a>
         </p>
         </div>
     </div>
@@ -44,15 +44,22 @@
       <!-- Section Title -->
       <div class="container">
         <div class="row justify-content-center">
-          <div class="col-xl-5 col-lg-6 col-md-6">
+          <div class="col-xl-6 col-lg-6 col-md-6">
             <div class="service-item position-relative">
+              {{ now()->diffInDays(\Carbon\Carbon::parse($sub->end_date)) }}
+            @if(\Carbon\Carbon::parse($sub->end_date) < now())
+            <h3 style="text:red">Anda Belum Berlangganan</h3>
+              <h5 class="my-3">Pilih Paket Untuk Menikmati Layanan Kami</h5>
+              <p> <a href="{{ route('customer/langganan.index') }}" class="btn btn-primary">langganan Sekarang</a> </p>
+             @elseif(\Carbon\Carbon::parse($sub->end_date) >= now() && $payment->status == "completed" )
               <h3>Data Langganan Anda</h3>
               <h2> {{ $sub->subscribepackage->name }}</h2>
               <p>Berakhir pada {{ \Carbon\Carbon::parse($sub->end_date)->format("d F Y") }}</p>
+              @endif
             </div>
           </div><!-- End Service Item -->
 
-          <div class="col-xl-5 col-lg-6 col-md-6">
+          <div class="col-xl-6 col-lg-6 col-md-6">
             <div class="service-item position-relative">
               <h3>Komisi Anda</h3>
               <h2 class="mb-3">Rp {{ $komisi }}</h2>
