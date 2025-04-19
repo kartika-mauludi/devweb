a3c42b6e61b1b8a37cbd6c99bfb8ee563f4eb1d8
yoga777 <yogaadikusuma9@gmail.com>
2025-04-19 18:53:48 +0700

    feat:(update get login data refer from extension)

--- app/Http/Controllers/AutoLoginController.php
+++ app/Http/Controllers/AutoLoginController.php
@@ -2,18 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\University;
 use Illuminate\Http\Request;
 use App\Models\UniversityAccount;
 use App\Models\UniversityWebsite;
+use Illuminate\Support\Facades\Log;
 
 class AutoLoginController extends Controller
 {
 
-    public function getLoginData()
+    public function getAllowedUrls()
     {
+        $urls = UniversityWebsite::pluck('url');
 
-        $akun = UniversityAccount::first();
+        return response()->json($urls);
+    }
 
+    public function getLoginData(Request $request)
+    {
+        $accessUrl = $request->header('access_url');
+        
+        $parsedUrl = parse_url($accessUrl);
+        
+        $baseUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'] . $parsedUrl['path'];
+        
+        $query = University::whereRaw("signin_url LIKE ?", [$baseUrl . '%']);
+        
+        $university = $query->first();
+    
+        if (!$university) {
+            return response()->json(['message' => 'University not found'], 404);
+        }
+
+        $akun = UniversityAccount::where('university_id', $university->id)->first();
+    
         return response()->json($akun);
     }
+    
+    
+
+    
+    
+    
 }
