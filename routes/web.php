e9a13d52ece22e046bc98604f7c9e28694a27a3a
Sarif Hidayatulloh <100738114+PieceofShid@users.noreply.github.com>
2025-05-26 23:43:04 +0700

    fix: middleware auth

--- routes/web.php
+++ routes/web.php
@@ -183,6 +183,7 @@ Route::resource('universities.websites', UniversityWebsiteController::class);
 
 
 // Jika menggunakan route API
+// Route::middleware('auth')->get('/api/get-login-data', [AutoLoginController::class, 'getLoginData']);
 Route::get('/api/get-login-data', [AutoLoginController::class, 'getLoginData']);
 Route::get('/api/get-allowed-urls', [AutoLoginController::class, 'getAllowedUrls']);
 
