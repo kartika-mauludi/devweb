78708be404595acfe7807880d270d23c18151d04
ludius01 <69234573+ludius01@users.noreply.github.com>
2025-06-27 10:37:24 +0700

    drop column user_id bonuses

--- database/migrations/2025_06_24_152423_drop_column_user_id_in_bonuses.php
+++ database/migrations/2025_06_24_152423_drop_column_user_id_in_bonuses.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('bonuses', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
             $table->dropColumn('user_id');
         });
     }
